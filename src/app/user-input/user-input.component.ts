807ad9f6db377776671c292e6b1701c833cd6c3e
ThomasFoydel <thomasjfoydel@gmail.com>
2023-03-16 14:16:00 -0400

    added weather api data fetch

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -7,9 +7,31 @@ import { Component } from '@angular/core';
 })
 export class UserInputComponent {
   city = '';
-  country = '';
+  days: any = [];
 
-  onSubmit() {
-    console.log('Form submitted:', this.city, this.country);
+  async onSubmit() {
+    const API_KEY = '5edaf9af814e0a53bb982ef8b401101a';
+    const latLonUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${this.city}&limit=1&appid=${API_KEY}`;
+    try {
+      const latLonResponse = await fetch(latLonUrl);
+      if (!latLonResponse.ok) throw new Error(latLonResponse.statusText);
+      const [latLonData] = await latLonResponse.json();
+      if (!latLonData) throw new Error('No lat lon data found');
+
+      const { lat, lon } = latLonData;
+
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+      const weatherResponse = await fetch(weatherUrl);
+      if (!weatherResponse.ok) throw new Error(weatherResponse.statusText);
+      const weatherData = await weatherResponse.json();
+      const { list } = weatherData;
+
+      const days: any[] = [];
+      for (let i = 0; i < list.length; i++) i % 8 === 0 && days.push(list[i]);
+
+      this.days = days;
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
   }
 }
