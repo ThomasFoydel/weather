2c0615af19ed5073c5fdd9f2be4c9fa2fa875a43
ThomasFoydel <thomasjfoydel@gmail.com>
2023-03-17 02:36:19 -0400

    updated date time display

--- src/app/date-time-display/date-time-display.component.ts
+++ src/app/date-time-display/date-time-display.component.ts
@@ -7,14 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DateTimeDisplayComponent implements OnInit {
   @Input() time: number | null = null;
-  timeString = '';
   dateString = '';
   weekday = '';
 
   ngOnInit() {
     if (this.time) {
       const newDate = new Date(this.time * 1000);
-      this.timeString = newDate.toLocaleTimeString();
       this.dateString = newDate.toLocaleDateString();
       this.weekday = newDate.toLocaleDateString('en-US', { weekday: 'long' });
     }
